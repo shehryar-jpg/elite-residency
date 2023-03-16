be3e5d4bb57de8f7d22fd7ecaf2e14b8f6c0a140
Shehryar Ahmed <shehryar7ahmed@gmail.com>
2023-03-16 15:48:04 +0500

    removed-button-around-hamburger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,19 +32,11 @@ const Navbar = () => {
             <div className="flex space-x-5">
               <p className="text-[#c5c5c5] text-sm cursor-pointer ">FEATURES</p>
               <p className="text-[#c5c5c5] text-sm  cursor-pointer">
-                RESOURCES
+                RESOURCESs
               </p>
             </div>
           </div>
-          {/* <button
-            data-collapse-toggle="navbar-default"
-            type="button"
-            
-            className="  inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-default"
-            aria-expanded="false"
-          > */}
-          {/* <span className="sr-only">Open main menu</span> */}
+
           <img
             onClick={() => {
               handleMobileNavToggle();
@@ -53,7 +45,6 @@ const Navbar = () => {
             alt=""
             className="h-16 "
           />
-          {/* </button> */}
         </Fade>
       </div>
       {toggleMobileNav ? (
